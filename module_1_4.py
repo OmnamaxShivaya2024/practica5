f61a43177653727938f90c1e55d91f41e4f0db45
Людмила Кузнецова <ananim2023@list.ru>
2024-08-10 23:07:10 +0300

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,6 +1,7 @@
 my_string=input("я строка из текста: ")
-symbol=input(len(my_string))
+print(len(my_string))
 print(my_string.upper())
 print(my_string.lower())
 print(my_string.replace(" ", ""))
-
+print(my_string[0])
+print(my_string[-1])
